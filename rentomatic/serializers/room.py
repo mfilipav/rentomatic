71bd01c317e5164af598c7474969c5f158c47e94
mfilipav <mfilipav@ethz.ch>
2022-07-18 00:44:21 +0200

    added RoomJsonEncoder, a custom encoder subclass to serialize dataclasses object into JSON, special UUID treatment

---
+++ rentomatic/serializers/room.py
@@ -0,0 +1,17 @@
+import json
+import dataclasses
+
+class RoomJsonEncoder(json.JSONEncoder):
+    def default(self, o):
+        # provide a custom encoder subclass to serialize dataclasses
+        # see https://docs.python.org/3/library/json.html#json.JSONEncoder.default
+        try:
+            new_dict = {}
+            if dataclasses.is_dataclass(o):
+                new_dict = dataclasses.asdict(o)
+
+                # UUID code is not directly JSON serialisable
+                new_dict['code'] = str(o.code)
+            return new_dict
+        except TypeError:
+            pass
---
+++ tests/serializers/test_room.py
@@ -0,0 +1,35 @@
+import json
+import uuid
+
+from rentomatic.serializers.room import RoomJsonEncoder
+from rentomatic.domain.room import Room
+
+
+def test_serialize_domain_room():
+    code = uuid.uuid4()
+
+    room = Room(
+        code=code,
+        size=200,
+        price=10,
+        longitude=-0.09998975,
+        latitude=51.75436293,
+    )
+    # double curly braces are used to avoid clashes
+    # with the f-string formatter
+    expected_json = f"""
+        {{
+            "code": "{code}",
+            "size": 200,
+            "price": 10,
+            "longitude": -0.09998975,
+            "latitude": 51.75436293
+        }}
+    """
+    # what does json.dumps take?
+    # Room object, and this class with the 'cls' kwark
+    # the latter overwrites JSONEncoder
+    # JSONEncoder can convert Python dict to JSON object
+    json_room = json.dumps(room, cls=RoomJsonEncoder)
+
+    assert json.loads(json_room) == json.loads(expected_json)
